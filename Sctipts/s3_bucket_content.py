18e8fd0bec65499c73c3b6abf9cd3ba51d4e3214
HeH+ <50049728+javierjrg@users.noreply.github.com>
2024-11-05 08:17:09 -0600

    Create s3_bucket_content.py
    Initial doc.
    
    This Python script uses the boto3 library to connect to an AWS S3 bucket named ggc-project-s3-bucket,
    	lists all objects within the specified prefix (greengrass_rpi4Project/1.0.0/), 
    	and prints each file's key (name) and its last modified date.

---
+++ Sctipts/s3_bucket_content.py
@@ -0,0 +1,14 @@
+"""
+Author: Javier J. Rodríguez
+Date: 2024-11-05
+Version: 1.0
+
+Description:
+	This Python script uses the boto3 library to connect to an AWS S3 bucket named ggc-project-s3-bucket,
+	lists all objects within the specified prefix (greengrass_rpi4Project/1.0.0/), 
+	and prints each file's key (name) and its last modified date.
+
+License: MIT License
+
+"""
+
